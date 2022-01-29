35e0ce4e965506140d72d5839ae59c74f80384a3
Igor.Dorozhkin <Igor.Dorozhkin@nedra.digital>
2022-01-30 00:47:11 +0300

    Fade the bar when the Player dies

--- scripts/GUI.gd
+++ scripts/GUI.gd
@@ -3,6 +3,21 @@ extends MarginContainer
 onready var number_label = $Bars/LifeBar/Count/Background/Number
 onready var bar = $Bars/LifeBar/TextureProgress
 onready var tween = $Tween
+onready var animated_health = 20
 
 func _ready():
-	pass
+		var player_max_health = $"../Characters/Player".max_health
+		bar.max_value = player_max_health
+
+func update_health(new_value):
+	tween.interpolate_property(self, "animated_health", animated_health, new_value, 0.6)
+	if not tween.is_active():
+		tween.start()
+
+func _on_Player_health_changed(player_health):
+	update_health(player_health)
+
+func _process(delta):
+		var round_value = round(animated_health)
+		number_label.text = str(round_value)
+		bar.value = round_value
